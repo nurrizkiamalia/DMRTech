c569401f50bad16b6581e52813c330079f2df508
nurrizkiamalia <rizkiamel9@gmail.com>
2024-05-13 14:56:07 +0700

    delete: background moving

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { DM_Sans, Raleway, Bricolage_Grotesque} from "next/font/google";
-import Image from "next/image";
-import planet from "@/public/assets/Planet.png"
 import "./globals.css";
 import MainHeader from "@/components/Header/MainHeader";
 import SocialMedia from "@/components/SocialMedia";
@@ -42,13 +40,6 @@ export default function RootLayout({
     <html lang="en" className="bg-dspBlack overflow-x-hidden w-screen" suppressHydrationWarning={true}>
       <body className={`${dmSans.variable} ${bricolage.variable} ${raleway.variable} text-white`}>
         <MainHeader />
-        <div className="fixed bg-black -z-10 opacity-80 w-screen h-screen flex justify-center items-center">
-          <Image 
-          src={planet}
-          alt="planet"
-          className="animate-spin-slow "
-          priority/>
-        </div>
         <SocialMedia />
         {children}
         <Footer />
