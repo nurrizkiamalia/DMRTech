e806d4998306102241c580febdc2420a31288b3e
nurrizkiamalia <rizkiamel9@gmail.com>
2024-05-21 15:03:28 +0700

    fix: large element for performance

--- app/ServiceCompany/components/ProjectShowcase/Projects.tsx
+++ app/ServiceCompany/components/ProjectShowcase/Projects.tsx
@@ -7,13 +7,17 @@ const Projects: React.FC = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
             {projectdata.map((item, index) => (
                 <div key={index} className="">
-                    <div className="overflow-hidden rounded-3xl w-full h-[300px] xl:h-[500px] relative bg-dspLightGray ">
+                    <div className="overflow-hidden rounded-3xl w-full h-[300px] xl:h-[500px] relative ">
                         <Image 
                         src={`/assets/${item.image}`}
-                        width={800}
-                        height={800}
+                        width={500}
+                        height={500}
                         alt="image"
-                        className=" absolute hover:scale-110 rounded-3xl transition-all ease-linear "
+                        className=" absolute hover:scale-110 rounded-3xl transition-all ease-linear"
+                        style={{
+                            height: "100%",
+                            width: "100%"
+                        }}
                         priority/>
                     </div>
                     <div className="flex flex-col lg:flex-row justify-between items-center pt-2 px-5">
